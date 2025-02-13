6ae4c625e286c633363f25d1ee786f1c5919ce3c
Adriano Valadar <adriano.valadar@outlook.com>
2025-02-13 00:30:57 +0000

    add LogOnlyError test and add using enum

--- tests/LoggerTests.cpp
+++ tests/LoggerTests.cpp
@@ -1,8 +1,10 @@
 #include "LoggerTests.h"
 #include <fstream>
 
+using enum LOGLEVEL;
+
 TEST_F(LoggerTests, LogMessageTest) {
-    loggerTests.log("Test message", LOGLEVEL::Info);
+    loggerTests.log("Test message", Info);
 
     std::stringstream buffer;
     buffer << logFile.rdbuf();
@@ -15,7 +17,7 @@ TEST_F(LoggerTests, MultipleLogMessagesTest) {
     constexpr int numberThreads{100};
     std::array<std::thread, numberThreads> threads;
     auto logMessage = [&]() {
-        loggerTests.log("Test multithreading message", LOGLEVEL::Info);
+        loggerTests.log("Test multithreading message", Info);
     };
     for (int i{}; i < threads.size(); i++)
         threads.at(i) = std::thread(logMessage);
@@ -28,4 +30,16 @@ TEST_F(LoggerTests, MultipleLogMessagesTest) {
 
     for (int i{}; i < threads.size(); i++)
         ASSERT_NE(std::string::npos, logContents.find("Test multithreading message"));
+}
+
+TEST_F(LoggerTests, LogOnlyError) {
+    logger.setMinimumLogLevel(Error);
+    loggerTests.log("Info message", Info);
+    loggerTests.log("Error message", Error);
+
+    std::stringstream buffer;
+    buffer << logFile.rdbuf();
+    std::string logContents = buffer.str();
+    ASSERT_EQ(std::string::npos, logContents.find("Info message"));
+    ASSERT_NE(std::string::npos, logContents.find("Error message"));
 }
