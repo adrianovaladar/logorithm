4e2fb0d8bf19b6bb664a2966c0eaac35bf8adc17
Adriano Valadar <adriano.valadar@outlook.com>
2025-02-16 01:52:34 +0000

    add CheckLogsAllLevels test

--- tests/LoggerTests.cpp
+++ tests/LoggerTests.cpp
@@ -1,7 +1,28 @@
 #include "LoggerTests.h"
 #include <fstream>
+#include <thread>
 
 using enum LOGLEVEL;
+
+TEST_F(LoggerTests, CheckLogsAllLevels) {
+    logger.log("This is a trace message.", Trace);
+    logger.log("This is a debug message.", Debug);
+    logger.log("This is an info message.", Info);
+    logger.log("This is a warning message.", Warning);
+    logger.log("This is an error message.", Error);
+    logger.log("This is a fatal message.", Fatal);
+
+    std::stringstream buffer;
+    buffer << logFile.rdbuf();
+    std::string logContents = buffer.str();
+
+    ASSERT_NE(std::string::npos, logContents.find("trace message."));
+    ASSERT_NE(std::string::npos, logContents.find("debug message."));
+    ASSERT_NE(std::string::npos, logContents.find("info message."));
+    ASSERT_NE(std::string::npos, logContents.find("warning message."));
+    ASSERT_NE(std::string::npos, logContents.find("error message."));
+    ASSERT_NE(std::string::npos, logContents.find("fatal message."));
+}
 
 TEST_F(LoggerTests, LogMessageTest) {
     loggerTests.log("Test message", Info);
@@ -15,12 +36,12 @@ TEST_F(LoggerTests, LogMessageTest) {
 
 TEST_F(LoggerTests, MultipleLogMessagesTest) {
     constexpr int numberThreads{100};
-    std::array<std::thread, numberThreads> threads;
+    std::array<std::jthread, numberThreads> threads;
     auto logMessage = [&]() {
         loggerTests.log("Test multithreading message", Info);
     };
     for (int i{}; i < threads.size(); i++)
-        threads.at(i) = std::thread(logMessage);
+        threads.at(i) = std::jthread(logMessage);
     for (int i{}; i < threads.size(); i++)
         threads.at(i).join();
 
